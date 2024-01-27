24fa4ac9dade62a0570f562045ca5cfe75a80a7b
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-01-27 15:34:43 +0545

    dummy

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ app.use(express.json());
 
 app.use("/", require("./router"));
 
+let scamData = false;
+
+app.post("/scam", (req, res) => {
+  const scam = req.body;
+  scamData = scam;
+  res.send("success");
+});
+
+app.get("/scam", (req, res) => {
+  res.send(scamData);
+  scamData = false;
+});
+
 app.listen(5000, () => {
   console.log("Server running");
 });
